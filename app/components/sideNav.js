af1b6f01ba2965d3c04f548338a93ff64ec51a69
JosiahOnline <joshdrewstudio@gmail.com>
2023-11-26 00:13:44 +0800

    Add Table Page

---
+++ app/components/sideNav.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+export default function SideNav() {
+  return (
+      <div className="row-span-6 col-span-1 py-10 flex flex-col justify-between">
+        <div>
+          <div className="text-green-800 text-center font-bold text-xl">
+            POS SYSTEM
+          </div>
+          <button className="bg-green-800 rounded-lg px-6 py-4 flex flex-col items-center mx-auto my-2 w-40">
+            <svg
+              className="w-10 h-10 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5h3m-6.75 2.25h10.5a2.25 2.25 0 002.25-2.25v-15a2.25 2.25 0 00-2.25-2.25H6.75A2.25 2.25 0 004.5 4.5v15a2.25 2.25 0 002.25 2.25z"
+              />
+            </svg>
+            <div className="text-white text-xl">Tables</div>
+          </button>
+          <button className="rounded-lg px-6 py-4 flex flex-col items-center m-auto my-2 w-40 group hover:bg-green-800">
+            <svg
+              className="w-10 h-10 mr-1 text-green-800 group-hover:text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+              />
+            </svg>
+            <div className="text-black text-xl group-hover:text-white">
+              Menu
+            </div>
+          </button>
+          <button className="rounded-lg px-6 py-4 flex flex-col items-center m-auto my-2 w-40 group hover:bg-green-800">
+            <svg
+              className="w-10 h-10 mr-1 text-green-800 group-hover:text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <div className="text-black text-xl group-hover:text-white">
+              Sales
+            </div>
+          </button>
+          <button className="rounded-lg px-6 py-4 flex flex-col items-center m-auto my-2 w-40 group hover:bg-green-800">
+            <svg
+              className="w-10 h-10 mr-1 text-green-800 group-hover:text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+            <div className="text-black text-xl group-hover:text-white">
+              Setting
+            </div>
+          </button>
+        </div>
+        <button className="rounded-lg px-6 py-4 flex flex-col items-center m-auto my-2 w-40 group hover:bg-green-800">
+          <svg
+            className="w-10 h-10 mr-1 text-green-800 group-hover:text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+            />
+          </svg>
+          <div className="text-black text-xl group-hover:text-white">
+            Logout
+          </div>
+        </button>
+      </div>
+  );
+}
---
+++ app/tables/page.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function Tables() {
+  return (
+    <div className="row-span-5 col-span-7 bg-gray-200 grid grid-cols-3 gap-10 grid-rows-4 py-10 px-10">
+      <button className="group hover:bg-yellow-500 bg-green-800 rounded-lg text-white flex items-center justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 1</div>
+        <div className="text-lg group-hover:text-black">Seated</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 2</div>
+        <div className="text-lg group-hover:text-black">Empty</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 3</div>
+        <div className="text-lg group-hover:text-black">Seated</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black  items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 4</div>
+        <div className="text-lg group-hover:text-black">Empty</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 5</div>
+        <div className="text-lg group-hover:text-black">Empty</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 6</div>
+        <div className="text-lg group-hover:text-black">Seated</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 7</div>
+        <div className="text-lg group-hover:text-black">Empty</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 8</div>
+        <div className="text-lg group-hover:text-black">Seated</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 9</div>
+        <div className="text-lg group-hover:text-black">Seated</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 10</div>
+        <div className="text-lg group-hover:text-black">Empty</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 11</div>
+        <div className="text-lg group-hover:text-black">Seated</div>
+      </button>
+      <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white  items-center flex justify-center flex-col">
+        <div className="text-xl group-hover:text-black">Table 12</div>
+        <div className="text-lg group-hover:text-black">Empty</div>
+      </button>
+    </div>
+  )
+}
