b2672b735f9e4ed906df0eeeac3f7dd391c4d166
JosiahOnline <joshdrewstudio@gmail.com>
2023-11-30 06:17:42 +0800

    Fixed Side Bar active tab color

--- app/components/sideNav.js
+++ app/components/sideNav.js
@@ -13,33 +13,33 @@ export default function SideNav() {
         </div>
         <Link
           href="/tables"
-          className={path === '/tables' ? "bg-green-800 rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group" : 'rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group'}>
+          className={path === '/tables' ? "bg-green-800 rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group" : 'rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6'}>
           <svg
-            className="w-8 h-8 text-green-800 group-hover:text-white"
+            className={path === '/tables' ? "w-8 h-8 text-white" : "w-8 h-8 text-green-800"}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor">
             <path d="M4.5 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM14.25 8.625a3.375 3.375 0 116.75 0 3.375 3.375 0 01-6.75 0zM1.5 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM17.25 19.128l-.001.144a2.25 2.25 0 01-.233.96 10.088 10.088 0 005.06-1.01.75.75 0 00.42-.643 4.875 4.875 0 00-6.957-4.611 8.586 8.586 0 011.71 5.157v.003z" />
           </svg>
-          <div className="text-black text-sm group-hover:text-white">吹水站</div>
+          <div className={path === '/tables' ? "text-white text-sm " : "text-black text-sm"}>吹水站</div>
         </Link>
         <Link
           href="/takeAway"
           className={path === '/takeAway' ? "bg-green-800 rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group" : 'rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group'}>
           <svg
-            className="w-8 h-8 text-green-800 group-hover:text-white"
+            className={path === '/takeAway' ? "w-8 h-8 text-white" : "w-8 h-8 text-green-800"}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor">
             <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
           </svg>
-          <div className="text-black text-sm group-hover:text-white">打包</div>
+          <div className={path === '/takeAway' ? "text-white text-sm " : "text-black text-sm"}>打包</div>
         </Link>
         <Link
           href="/salesReport"
           className={path === '/salesReport' ? "bg-green-800 rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group" : 'rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group'}>
           <svg
-            className="w-8 h-8 text-green-800 group-hover:text-white"
+            className={path === '/salesReport' ? "w-8 h-8 text-white" : "w-8 h-8 text-green-800"}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor">
@@ -50,7 +50,7 @@ export default function SideNav() {
               clipRule="evenodd"
             />
           </svg>
-          <div className="text-black text-sm group-hover:text-white text-center">
+          <div className={path === '/salesReport' ? "text-white text-sm " : "text-black text-sm"}>
             招財貓
           </div>
         </Link>
@@ -58,7 +58,7 @@ export default function SideNav() {
           href="/setting"
           className={path === '/setting' ? "bg-green-800 rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group" : 'rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group'}>
           <svg
-            className="w-8 h-8 text-green-800 group-hover:text-white"
+            className={path === '/setting' ? "w-8 h-8 text-white" : "w-8 h-8 text-green-800"}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor">
@@ -74,7 +74,7 @@ export default function SideNav() {
               clipRule="evenodd"
             />
           </svg>
-          <div className="text-black text-sm group-hover:text-white">
+          <div className={path === '/setting' ? "text-white text-sm " : "text-black text-sm"}>
             唔好碰
           </div>
         </Link>
@@ -84,7 +84,7 @@ export default function SideNav() {
           href="/"
           className={path === '/' ? "bg-green-800 rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group" : 'rounded-lg py-4 px-4 flex flex-col items-center mx-auto my-1 w-5/6 group'}>
           <svg
-            className="w-8 h-8 text-green-800 group-hover:text-white"
+            className={path === '/' ? "w-8 h-8 text-white" : "w-8 h-8 text-green-800"}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor">
@@ -94,7 +94,7 @@ export default function SideNav() {
               clipRule="evenodd"
             />
           </svg>
-          <div className="text-black text-sm group-hover:text-white">走后门</div>
+          <div className={path === '/' ? "text-white text-sm " : "text-black text-sm"}>走后门</div>
         </Link>
       </div>
     </div>
--- app/tables/page.js
+++ app/tables/page.js
@@ -9,53 +9,53 @@ export default function Tables() {
           Please Select Table
         </div>
         <div className="grid grid-cols-3 gap-10 grid-rows-5 ">
-          <button className="group hover:bg-yellow-500 bg-green-800 rounded-lg text-white flex items-center justify-center flex-col py-4">
-            <div className="text-md group-hover:text-black">Table 1</div>
-            <div className="text-sm group-hover:text-black">Seated</div>
+          <button className="group hover:bg-green-800 bg-yellow-500 rounded-lg text-white flex items-center justify-center flex-col py-4">
+            <div className="text-md group-hover:text-white">Table 1</div>
+            <div className="text-sm group-hover:text-white">Seated</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 2</div>
-            <div className="text-sm group-hover:text-black">Empty</div>
+          <button className="group  hover:bg-green-800 bg-white rounded-lg text-black items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 2</div>
+            <div className="text-sm group-hover:text-white">Empty</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 3</div>
-            <div className="text-sm group-hover:text-black">Seated</div>
+          <button className="group  hover:bg-green-800 bg-yellow-500 rounded-lg text-white items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 3</div>
+            <div className="text-sm group-hover:text-white">Seated</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black  items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 4</div>
-            <div className="text-sm group-hover:text-black">Empty</div>
+          <button className="group  hover:bg-green-800 bg-white rounded-lg text-black  items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 4</div>
+            <div className="text-sm group-hover:text-white">Empty</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 5</div>
-            <div className="text-sm group-hover:text-black">Empty</div>
+          <button className="group  hover:bg-green-800 bg-white rounded-lg text-black items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 5</div>
+            <div className="text-sm group-hover:text-white">Empty</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 6</div>
-            <div className="text-sm group-hover:text-black">Seated</div>
+          <button className="group  hover:bg-green-800 bg-yellow-500 rounded-lg text-white items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 6</div>
+            <div className="text-sm group-hover:text-white">Seated</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 7</div>
-            <div className="text-sm group-hover:text-black">Empty</div>
+          <button className="group  hover:bg-green-800 bg-white rounded-lg text-black items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 7</div>
+            <div className="text-sm group-hover:text-white">Empty</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 8</div>
-            <div className="text-sm group-hover:text-black">Seated</div>
+          <button className="group  hover:bg-green-800 bg-yellow-500 rounded-lg text-white items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 8</div>
+            <div className="text-sm group-hover:text-white">Seated</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 9</div>
-            <div className="text-sm group-hover:text-black">Seated</div>
+          <button className="group  hover:bg-green-800 bg-yellow-500 rounded-lg text-white items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 9</div>
+            <div className="text-sm group-hover:text-white">Seated</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-white rounded-lg text-black items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 10</div>
-            <div className="text-sm group-hover:text-black">Empty</div>
+          <button className="group  hover:bg-green-800 bg-white rounded-lg text-black items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 10</div>
+            <div className="text-sm group-hover:text-white">Empty</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 11</div>
-            <div className="text-sm group-hover:text-black">Seated</div>
+          <button className="group  hover:bg-green-800 bg-yellow-500 rounded-lg text-white items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 11</div>
+            <div className="text-sm group-hover:text-white">Seated</div>
           </button>
-          <button className="group  hover:bg-yellow-500 bg-green-800 rounded-lg text-white  items-center flex justify-center flex-col">
-            <div className="text-md group-hover:text-black">Table 12</div>
-            <div className="text-sm group-hover:text-black">Empty</div>
+          <button className="group  hover:bg-green-800 bg-yellow-500 rounded-lg text-white  items-center flex justify-center flex-col">
+            <div className="text-md group-hover:text-white">Table 12</div>
+            <div className="text-sm group-hover:text-white">Empty</div>
           </button>
         </div>
       </div>
