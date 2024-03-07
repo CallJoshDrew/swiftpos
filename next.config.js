750225ac4b74846d56e1682906d83f57226eafeb
JosiahOnline <joshdrewstudio@gmail.com>
2024-03-07 21:45:05 +0800

    Update next.config.js because of PWA

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 // next.config.js
-const withPWA = require("next-pwa");
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  skipWaiting: true,
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     minimumCacheTTL: 60,
     // 86400, // Cache duration in seconds (24 hours)
-  },
-  pwa: {
-    dest: "public",
-    register: true,
-    disable: process.env.NODE_ENV === "development",
-    skipWaiting: true,
   },
 };
 
