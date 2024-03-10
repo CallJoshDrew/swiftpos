77022a7d06593a530fcd878e6d825f64c6d33816
JosiahOnline <joshdrewstudio@gmail.com>
2024-03-10 16:17:04 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <meta name="application-name" content="JSPOS" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="JSPOS" />
+          <meta name="description" content="he fastest growing of POS system which improve your operations performance according to your working culture! This is from Sabah, a local entrepreneur aims to make a big differences in the space of technologies and businesses." />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          {/* <meta name="msapplication-config" content="/icons/browserconfig.xml" /> */}
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/images/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
@@ -17,8 +28,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
