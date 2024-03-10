02c81285229a80455968d929a545f6256d1327b4
JosiahOnline <joshdrewstudio@gmail.com>
2024-03-10 17:02:10 +0800

    Update _documents

--- pages/_document.js
+++ pages/_document.js
@@ -5,23 +5,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="application-name" content="JSPOS" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-          <meta name="apple-mobile-web-app-title" content="JSPOS" />
-          <meta name="description" content="he fastest growing of POS system which improve your operations performance according to your working culture! This is from Sabah, a local entrepreneur aims to make a big differences in the space of technologies and businesses." />
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          {/* <meta name="msapplication-config" content="/icons/browserconfig.xml" /> */}
-          <meta name="msapplication-TileColor" content="#2B5797" />
-          <meta name="msapplication-tap-highlight" content="no" />
-          <meta name="theme-color" content="#000000" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="shortcut icon" href="/images/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="192x192"  href="/android-chrome-192x192.png" />
+          <link rel="icon" type="image/png" sizes="512x512"  href="/android-chrome-512x512.png" />
           {/* Other Head elements like title, meta tags etc. */}
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
         </Head>
         <body>
           <Main />
